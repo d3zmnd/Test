1673c60ad03ca92355ed1df577909a755cb3df4d
Vitaly Davidenko <35844879+d3zmnd@users.noreply.github.com>
2019-03-27 19:26:33 +0200

    Add files via upload
    Stable version. Begin to break into components

---
+++ components/Controls.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import { Asset, Audio } from 'expo';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+export default class Controls extends Component {
+
+  render() {
+    return (
+          <View
+            style={[
+              styles.buttonsContainerBase,
+              styles.buttonsContainerTopRow,
+              {
+                opacity: this.state.isLoading
+                  ? DISABLED_OPACITY
+                  : 1.0,
+              },
+            ]}
+          >
+            <TouchableHighlight
+              underlayColor={BACKGROUND_COLOR}
+              style={styles.wrapper}
+              onPress={this._onBackPressed}
+              disabled={this.state.isLoading}
+            >
+              <View>
+                <MaterialIcons
+                  name="fast-rewind"
+                  size={40}
+                  color="#56D5FA"
+                />
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight
+              underlayColor={BACKGROUND_COLOR}
+              style={styles.wrapper}
+              onPress={this._onPlayPausePressed}
+              disabled={this.state.isLoading}
+            >
+              <View>
+                {this.state.isPlaying ? (
+                  <MaterialIcons
+                    name="pause"
+                    size={40}
+                    color="#56D5FA"
+                  />
+                ) : (
+                    <MaterialIcons
+                      name="play-arrow"
+                      size={40}
+                      color="#56D5FA"
+                    />
+                  )}
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight
+              underlayColor={BACKGROUND_COLOR}
+              style={styles.wrapper}
+              onPress={this._onStopPressed}
+              disabled={this.state.isLoading}
+            >
+              <View>
+                <MaterialIcons
+                  name="stop"
+                  size={40}
+                  color="#56D5FA"
+                />
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight
+              underlayColor={BACKGROUND_COLOR}
+              style={styles.wrapper}
+              onPress={this._onForwardPressed}
+              disabled={this.state.isLoading}
+            >
+              <View>
+                <MaterialIcons
+                  name="fast-forward"
+                  size={40}
+                  color="#56D5FA"
+                />
+              </View>
+            </TouchableHighlight>
+          </View>          
+      );
+  }
+}
+
+const styles = StyleSheet.create({  
+  buttonsContainerTopRow: {
+    maxHeight: 40,
+    minWidth: DEVICE_WIDTH / 2.0,
+    maxWidth: DEVICE_WIDTH / 2.0,
+  },  
+  wrapper:{
+    flex: 1,
+    alignItems: 'center',
+  },
+});
